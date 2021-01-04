5a11657c6b62b0e556c078104930e4ce7b3d4d70
Florian <67049493+Florian2301@users.noreply.github.com>
2021-01-04 18:04:04 +0100

    Verschönerung

--- client/src/main/chat/Title.js
+++ client/src/main/chat/Title.js
@@ -11,19 +11,24 @@ export function Title (props) {
           	<span id="span-number">
              <strong>No.: </strong>
             </span>
+            
             <span id="span-props-number">
             {props.chat.chatEditmode? props.chat.chatnumber : null}
             </span>
+            
             <span id="span-title">
              <strong>Title: </strong>
             </span>
+            
             <span id="span-props-title">
             {props.draft.draftEditmode? props.draft.title : null}
             {props.chat.chatEditmode? props.chat.title : null}
             </span>
+            
             <span id="span-date">
              <strong>Date: </strong>
             </span>
+            
             <span id="span-props-date">
             {props.draft.draftEditmode? props.draft.date : null}
             {props.chat.chatEditmode? props.chat.date : null}
