f19de2cc796217187c3d7f9ca4cb568c2346bec3
Florian <67049493+Florian2301@users.noreply.github.com>
2021-01-10 21:50:24 +0100

    toggle verbessert

--- client/src/responsive/MobileSitemap.js
+++ client/src/responsive/MobileSitemap.js
@@ -4,15 +4,35 @@ import { Collapse } from 'react-bootstrap'
 import Panel from '../elements/Panel'
 import { setKey } from '../redux/actions/user'
 
+
 export function MobileSitemap(props) {
-  const [details, setDetails] = useState("")
+  const [about, setAbout] = useState(false)
+  const [history, setHistory] = useState(false)
+  const [userchats, setUserchats] = useState(false)
+  const [chat, setChat] = useState(false)
+  const [login, setLogin] = useState(false)
+  const [drafts, setDrafts] = useState(false)
+  const [publish, setPublish] = useState(false)
+
+  function toggleDetails(key) {
+    setAbout(false)
+    setHistory(false)
+    setUserchats(false)
+    setChat(false)
+    setLogin(false)
+    setDrafts(false)
+    setPublish(false)
+    if(key === "about") setAbout(true)
+    if(key === "history") setHistory(true)
+    if(key === "userchats") setUserchats(true)
+    if(key === "chat") setChat(true)
+    if(key === "login") setLogin(true)
+    if(key === "drafts") setDrafts(true)
+    if(key === "publish") setPublish(true)
+  }
 
   function handleKey(key) {
       props.setKey(key)
-  }
-  
-  function toggleDetails(key) {
-    setDetails(key)
   }
   
   //------------------------- RETURN --------------------------------------------------------------------------
@@ -24,9 +44,9 @@ export function MobileSitemap(props) {
             <div>
               <div className="menu-panel">
             <p className="start-para"><span className="menu" onClick={() => handleKey("about")}> About </span></p>
-            <p className="menu-link" onClick={() => toggleDetails("about")} aria-controls="example-collapse-text" aria-expanded={details}>{!details? "more..." : "less..."}</p>
+            <p className="menu-link" onClick={() => toggleDetails("about")} aria-controls="example-collapse-text" aria-expanded={about}>{!about? "more..." : "less..."}</p>
               </div>
-              <Collapse in={details === "about"}>
+              <Collapse in={about}>
                 <div className="menu-details" id="example-collapse-text">
                 <p className="start-para-details">Finally, on the last page <span className="menu-inside" onClick={() => handleKey("about")}>"About" </span>,  
                 you will find a short explanation about why i created this app, get more technical informations about it and see the code behind (on github).</p>
@@ -37,9 +57,9 @@ export function MobileSitemap(props) {
             <div>
               <div className="menu-panel">
                 <p className="start-para"><span className="menu" onClick={() => handleKey("history")}> History </span></p>
-                <p className="menu-link" onClick={() => toggleDetails("history")} aria-controls="example-collapse-text" aria-expanded={details}>{!details? "more..." : "less..."}</p>
+                <p className="menu-link" onClick={() => toggleDetails("history")} aria-controls="example-collapse-text" aria-expanded={history}>{!history? "more..." : "less..."}</p>
               </div>
-              <Collapse in={details === "history"}>
+              <Collapse in={history}>
                 <div className="menu-details" id="example-collapse-text">
                   <p className="start-para-details">If you click on <span className="menu-inside" onClick={() => handleKey("history")}>"History" </span>
                   you can see some example dialogues i quoted from the great Plato, to see how a chat will look like. 
@@ -51,9 +71,9 @@ export function MobileSitemap(props) {
             <div>
               <div className="menu-panel">
                 <p className="start-para"><span className="menu" onClick={() => handleKey("userchats")}> Userchats </span></p>
-                <p className="menu-link" onClick={() => toggleDetails("userchats")} aria-controls="example-collapse-text" aria-expanded={details}>{!details? "more..." : "less..."}</p>
+                <p className="menu-link" onClick={() => toggleDetails("userchats")} aria-controls="example-collapse-text" aria-expanded={userchats}>{!userchats? "more..." : "less..."}</p>
               </div>
-              <Collapse in={details === "userchats"}>
+              <Collapse in={userchats}>
                 <div className="menu-details" id="example-collapse-text">
                   <p className="start-para-details">On <span className="menu-inside" onClick={() => handleKey("userchats")}>"Userchats" </span> you can find posts from users 
                   (at the moment you find there also a quote of Plato).</p>
@@ -64,9 +84,9 @@ export function MobileSitemap(props) {
             <div>
               <div className="menu-panel">
                 <p className="start-para"><span className="menu" onClick={() => handleKey("chat")}> Chat </span></p>
-                <p className="menu-link" onClick={() => toggleDetails("chat")} aria-controls="example-collapse-text" aria-expanded={details}>{!details? "more..." : "less..."}</p>
+                <p className="menu-link" onClick={() => toggleDetails("chat")} aria-controls="example-collapse-text" aria-expanded={chat}>{!chat? "more..." : "less..."}</p>
               </div>
-              <Collapse in={details === "chat"}>
+              <Collapse in={chat}>
                 <div className="menu-details" id="example-collapse-text">
                   <p className="start-para-details">On <span className="menu-inside" onClick={() => handleKey("chat")}>"Chat" </span> the saved chats will be displayed, 
                   here you will also write your own chats.</p>
@@ -77,9 +97,9 @@ export function MobileSitemap(props) {
             <div>
               <div className="menu-panel">
                 <p className="start-para"><span className="menu" onClick={() => handleKey("login")}> Login / Logout</span></p>
-                <p className="menu-link" onClick={() => toggleDetails("login")} aria-controls="example-collapse-text" aria-expanded={details}>{!details? "more..." : "less..."}</p>
+                <p className="menu-link" onClick={() => toggleDetails("login")} aria-controls="example-collapse-text" aria-expanded={login}>{!login? "more..." : "less..."}</p>
               </div>
-              <Collapse in={details === "login"}>
+              <Collapse in={login}>
                 <div className="menu-details" id="example-collapse-text">
                   <p className="start-para-details">If you like to try this text editor you can go to <span className="menu-inside" onClick={() => handleKey("login")}>"Login" </span>. 
                   There you can either just click "login" and try the testuser account or create your own profile by signing up</p>
@@ -90,9 +110,9 @@ export function MobileSitemap(props) {
             <div>
               <div className="menu-panel">
                 <p className="start-para"><span className="menu" onClick={() => handleKey("drafts")}> Drafts </span></p>
-                <p className="menu-link" onClick={() => toggleDetails("drafts")} aria-controls="example-collapse-text" aria-expanded={details}>{!details? "more..." : "less..."}</p>
+                <p className="menu-link" onClick={() => toggleDetails("drafts")} aria-controls="example-collapse-text" aria-expanded={drafts}>{!drafts? "more..." : "less..."}</p>
               </div>
-              <Collapse in={details === "drafts"}>
+              <Collapse in={drafts}>
                 <div className="menu-details" id="example-collapse-text">
                   <p className="start-para-details">if you are logged in you can start writing a chat (either as testuser or as your own profile). Just go to  
                   <span className="menu-inside" onClick={() => handleKey("drafts")}> "Drafts" </span>, set up at least one name and write your first lines. 
@@ -104,9 +124,9 @@ export function MobileSitemap(props) {
             <div>
               <div className="menu-panel">
                 <p className="start-para"><span className="menu" onClick={() => handleKey("publish")}> Publish </span></p>
-                <p className="menu-link" onClick={() => toggleDetails("publish")} aria-controls="example-collapse-text" aria-expanded={details}>{!details? "more..." : "less..."}</p>
+                <p className="menu-link" onClick={() => toggleDetails("publish")} aria-controls="example-collapse-text" aria-expanded={publish}>{!publish? "more..." : "less..."}</p>
               </div>
-              <Collapse in={details === "publish"}>
+              <Collapse in={publish}>
                 <div className="menu-details" id="example-collapse-text">
                   <p className="start-para-details">Once you are ready to publish your chat you go to <span className="menu-inside" onClick={() => handleKey("publish")}>"Publish" </span>, 
                   there you can edit/add more detailed informations to your chat and publish them, so everyone can read what you have written 
