f20f1b5d37158bfe06a0cdb596723e35e448613b
Florian <67049493+Florian2301@users.noreply.github.com>
2021-01-12 21:44:55 +0100

    buttons to philosopher

--- client/src/redux/reducers/draftReducer.js
+++ client/src/redux/reducers/draftReducer.js
@@ -6,7 +6,7 @@ let initialState = {
   date: "",
   tags: [],
   description: "",
-  buttons: [],
+  philosopher: [],
   messages: [],
   adminDrafts: [],
   userDrafts: [],
@@ -16,9 +16,9 @@ let initialState = {
 function drafts(state = initialState, action) {
   switch (action.type) {
     case "START_CHAT": 
-      return {...state, buttons: action.names}
+      return {...state, philosopher: action.names}
     case "REMOVE_NAME":
-      return {...state, buttons: state.buttons.filter(button => button.name !== action.name.name)}
+      return {...state, philosopher: state.philosopher.filter(phil => phil.name !== action.name.name)}
     case "GET_DRAFT":
       return {...state,
               draftId: action.payload._id,
@@ -28,7 +28,7 @@ function drafts(state = initialState, action) {
               date: action.payload.date,
               tags: action.payload.tags,
               description: action.payload.description, 
-              buttons: action.payload.buttons, 
+              philosopher: action.payload.philosopher, 
               messages: action.payload.messages,
               draftEditmode: true
             }
@@ -52,7 +52,7 @@ function drafts(state = initialState, action) {
                 date: "",
                 tags: [],
                 description: "",
-                buttons: [],
+                philosopher: [],
                 messages: [],
                 draftEditmode: false}
     case "DELETE_USER_DRAFT":
