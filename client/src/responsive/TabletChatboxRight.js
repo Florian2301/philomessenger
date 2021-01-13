2a034f5d62903174df3b36c3b4f9dcffcbd7eb96
Florian <67049493+Florian2301@users.noreply.github.com>
2021-01-13 18:34:40 +0100

    tablet border color

--- client/src/responsive/TabletChatboxRight.js
+++ client/src/responsive/TabletChatboxRight.js
@@ -15,6 +15,7 @@ import TabletSitemap from './TabletSitemap'
 export function TabletChatboxRight(props) {
 
   return (
+    <div id="responsive-border-tablet-right">
     <Container fluid id="responsive-container-tablet">
       <Tabs defaultActiveKey={"sitemap"} id="uncontrolled" style={{borderBottom: 0}}>
           
@@ -50,6 +51,7 @@ export function TabletChatboxRight(props) {
           
       </Tabs>
     </Container>
+    </div>
   )
 }
 
