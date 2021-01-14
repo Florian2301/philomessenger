dc22d6424e500223c40d7558e13b33407f9dc765
Florian <67049493+Florian2301@users.noreply.github.com>
2021-01-14 21:07:47 +0100

    Text überarbeitet

--- client/src/responsive/MobileSitemap.js
+++ client/src/responsive/MobileSitemap.js
@@ -38,104 +38,107 @@ export function MobileSitemap(props) {
   //------------------------- RETURN --------------------------------------------------------------------------
     
     return (
-        <Panel title="The structure of this app is as follows" id="start-panel">            
-            <p className="start-para" id="menu-advice-mobile">- recommended view is on a laptop/desktop screen -</p>
-
-            <div>
-              <div className="menu-panel">
-            <p className="start-para"><span className="menu" onClick={() => handleKey("about")}> About </span></p>
-            <p className="menu-link" onClick={() => toggleDetails("about")} aria-controls="example-collapse-text" aria-expanded={about}>{!about? "more..." : "less..."}</p>
-              </div>
-              <Collapse in={about}>
-                <div className="menu-details" id="example-collapse-text">
-                <p className="start-para-details">Finally, on the last page <span className="menu-inside" onClick={() => handleKey("about")}>"About" </span>,  
-                you will find a short explanation about why i created this app, get more technical informations about it and see the code behind (on github).</p>
-                </div>
-              </Collapse>
-            </div>
+        <Panel title="Your guide of this app" id="start-panel">            
+            <p className="start-para-mobile" id="menu-advice-mobile">- recommended view is on a laptop/desktop screen -</p>
             
             <div>
               <div className="menu-panel">
-                <p className="start-para"><span className="menu" onClick={() => handleKey("history")}> History </span></p>
+                <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("history")}> History </span></p>
                 <p className="menu-link" onClick={() => toggleDetails("history")} aria-controls="example-collapse-text" aria-expanded={history}>{!history? "more..." : "less..."}</p>
               </div>
               <Collapse in={history}>
                 <div className="menu-details" id="example-collapse-text">
-                  <p className="start-para-details">If you click on <span className="menu-inside" onClick={() => handleKey("history")}>"History" </span>
-                  you can see some example dialogues i quoted from the great Plato, to see how a chat will look like. 
-                  In this section i will post my own chats</p>
+                  <p className="start-para-details">Under <span className="menu-inside" onClick={() => handleKey("history")}>"History" </span>
+                  you will find an introduction about how to use this text editor. In this section i will also post my own chats in the future.</p>
+                  <p className="start-para-details">If you click on the title of a chat it will be displayed in <span className="menu-inside" onClick={() => handleKey("chat")}>"Chat" </span>.</p>
+                  <p className="start-para-details">After you clicked the title, the "date" will change to a download link where you can download the chat as a PDF document.</p>
+                  <p className="start-para-details">If you only hover over the title additional informations about the chat will appear: Tags and a brief description of the chat.</p>
                 </div>
               </Collapse>
             </div>
             
             <div>
               <div className="menu-panel">
-                <p className="start-para"><span className="menu" onClick={() => handleKey("userchats")}> Userchats </span></p>
+                <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("userchats")}> Userchats </span></p>
                 <p className="menu-link" onClick={() => toggleDetails("userchats")} aria-controls="example-collapse-text" aria-expanded={userchats}>{!userchats? "more..." : "less..."}</p>
               </div>
               <Collapse in={userchats}>
                 <div className="menu-details" id="example-collapse-text">
-                  <p className="start-para-details">On <span className="menu-inside" onClick={() => handleKey("userchats")}>"Userchats" </span> you can find posts from users 
-                  (at the moment you find there also a quote of Plato).</p>
+                  <p className="start-para-details">Under <span className="menu-inside" onClick={() => handleKey("userchats")}>"Userchats" </span> you can publish your own chats after registration. 
+                  At the moment you can see example dialogues i quoted from the great Plato, to see how your chat could look like.</p>
                 </div>
               </Collapse>
             </div>
             
             <div>
               <div className="menu-panel">
-                <p className="start-para"><span className="menu" onClick={() => handleKey("chat")}> Chat </span></p>
+                <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("chat")}> Chat </span></p>
                 <p className="menu-link" onClick={() => toggleDetails("chat")} aria-controls="example-collapse-text" aria-expanded={chat}>{!chat? "more..." : "less..."}</p>
               </div>
               <Collapse in={chat}>
                 <div className="menu-details" id="example-collapse-text">
-                  <p className="start-para-details">On <span className="menu-inside" onClick={() => handleKey("chat")}>"Chat" </span> the saved chats will be displayed, 
-                  here you will also write your own chats.</p>
+                  <p className="start-para-details">On <span className="menu-inside" onClick={() => handleKey("chat")}>"Chat" </span> the published chats will be displayed. When you are logged in, you will 
+                  write your chats in this section as well.</p>
                 </div>
               </Collapse>
             </div>
 
             <div>
               <div className="menu-panel">
-                <p className="start-para"><span className="menu" onClick={() => handleKey("login")}> Login / Logout</span></p>
+                <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("login")}> Login/Logout</span></p>
                 <p className="menu-link" onClick={() => toggleDetails("login")} aria-controls="example-collapse-text" aria-expanded={login}>{!login? "more..." : "less..."}</p>
               </div>
               <Collapse in={login}>
                 <div className="menu-details" id="example-collapse-text">
-                  <p className="start-para-details">If you like to try this text editor you can go to <span className="menu-inside" onClick={() => handleKey("login")}>"Login" </span>. 
-                  There you can either just click "login" and try the testuser account or create your own profile by signing up</p>
+                  <p className="start-para-details">If you like to have a look "inside" the text editor, to see how to use it, you can go to <span className="menu-inside" onClick={() => handleKey("login")}>"Login" </span> and 
+                  log in with the given testuser credentials. Feel free to write, edit and publish a chat for testing purposes.</p>
+                  <p className="start-para-details">The authorization is at the moment not a safe "https" process, a safe way to log in with your private email address will available in the future. 
+                  This app is only for demonstration hosted at "heroku.com".</p>
                 </div>
               </Collapse>
             </div>
 
             <div>
               <div className="menu-panel">
-                <p className="start-para"><span className="menu" onClick={() => handleKey("drafts")}> Drafts </span></p>
-                <p className="menu-link" onClick={() => toggleDetails("drafts")} aria-controls="example-collapse-text" aria-expanded={drafts}>{!drafts? "more..." : "less..."}</p>
+            <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("drafts")}> Drafts </span></p>
+            <p className="menu-link" onClick={() => toggleDetails("drafts")} aria-controls="example-collapse-text" aria-expanded={drafts}>{!drafts? "more..." : "less..."}</p>
               </div>
               <Collapse in={drafts}>
                 <div className="menu-details" id="example-collapse-text">
-                  <p className="start-para-details">if you are logged in you can start writing a chat (either as testuser or as your own profile). Just go to  
-                  <span className="menu-inside" onClick={() => handleKey("drafts")}> "Drafts" </span>, set up at least one name and write your first lines. 
-                  Afterwards you can add more details to your draft like a title and description and save it to your account.</p>
+                  <p className="start-para-details">if you log in you can start writing a chat in the <span className="menu-inside" onClick={() => handleKey("drafts")}> "Drafts" </span> section.</p>
+                  <p className="start-para-details">First add names (your participants), set a title and click "New chat". Afterwards you can start writing your own chat.</p>
+                  <p className="start-para-details">If you like to edit the draft of your chat you can click "Save changes" when you are done editing, changes will be saved.</p>
+                  <p className="start-para-details">"Tags" and "Description" are the parts which will appear on the <span className="menu-inside" onClick={() => handleKey("userchats")}>"Userchats" </span> section when you published your chat.</p>
                 </div>
               </Collapse>
             </div>
 
             <div>
               <div className="menu-panel">
-                <p className="start-para"><span className="menu" onClick={() => handleKey("publish")}> Publish </span></p>
+                <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("publish")}> Publish </span></p>
                 <p className="menu-link" onClick={() => toggleDetails("publish")} aria-controls="example-collapse-text" aria-expanded={publish}>{!publish? "more..." : "less..."}</p>
               </div>
               <Collapse in={publish}>
                 <div className="menu-details" id="example-collapse-text">
                   <p className="start-para-details">Once you are ready to publish your chat you go to <span className="menu-inside" onClick={() => handleKey("publish")}>"Publish" </span>, 
-                  there you can edit/add more detailed informations to your chat and publish them, so everyone can read what you have written 
-                  (must be logged in to publish your texts, see above for login).</p>
+                  there you have to set a "chatnumber" and a "date", then click on "Publish chat", it will be displayed in <span className="menu-inside" onClick={() => handleKey("userchats")}>"Userchats" </span>.</p>
+                  <p className="start-para-details">You can also change the "chatnumber" "title", "date", "tags" and "description" of a published chat when you click on "Save changes" after you you made your changes.</p>
                 </div>
               </Collapse>
             </div>
 
-
+            <div>
+              <div className="menu-panel">
+                <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("about")}> About </span></p>
+                <p className="menu-link" onClick={() => toggleDetails("about")} aria-controls="example-collapse-text" aria-expanded={about}>{!about? "more..." : "less..."}</p>
+              </div>
+              <Collapse in={about}>
+                <div className="menu-details" id="example-collapse-text">
+                <p className="start-para-details">On <span className="menu-inside" onClick={() => handleKey("about")}>"About" </span> you will find a short explanation  
+                about why i created this app, get more technical informations about it and see the code behind on <a id="git" href="https://github.com/Florian2301/philomessenger">Github</a>.</p>
+                </div>
+              </Collapse>
+            </div>
         </Panel>
     )
 }
