26968a720aa21bb53433960358cdfcf2be80fbee
Florian <67049493+Florian2301@users.noreply.github.com>
2021-01-15 10:49:55 +0100

    Text verkürzt

--- client/src/responsive/MobileSitemap.js
+++ client/src/responsive/MobileSitemap.js
@@ -50,9 +50,7 @@ export function MobileSitemap(props) {
                 <div className="menu-details" id="example-collapse-text">
                   <p className="start-para-details">Under <span className="menu-inside" onClick={() => handleKey("history")}>"History" </span>
                   you will find an introduction about how to use this text editor. In this section i will also post my own chats in the future.</p>
-                  <p className="start-para-details">If you click on the title of a chat it will be displayed in <span className="menu-inside" onClick={() => handleKey("chat")}>"Chat" </span>.</p>
                   <p className="start-para-details">After you clicked the title, the "date" will change to a download link where you can download the chat as a PDF document.</p>
-                  <p className="start-para-details">If you only hover over the title additional informations about the chat will appear: Tags and a brief description of the chat.</p>
                 </div>
               </Collapse>
             </div>
@@ -92,8 +90,6 @@ export function MobileSitemap(props) {
                 <div className="menu-details" id="example-collapse-text">
                   <p className="start-para-details">If you like to have a look "inside" the text editor, to see how to use it, you can go to <span className="menu-inside" onClick={() => handleKey("login")}>"Login" </span> and 
                   log in with the given testuser credentials. Feel free to write, edit and publish a chat for testing purposes.</p>
-                  <p className="start-para-details">The authorization is at the moment not a safe "https" process, a safe way to log in with your private email address will available in the future. 
-                  This app is only for demonstration hosted at "heroku.com".</p>
                 </div>
               </Collapse>
             </div>
@@ -107,8 +103,7 @@ export function MobileSitemap(props) {
                 <div className="menu-details" id="example-collapse-text">
                   <p className="start-para-details">if you log in you can start writing a chat in the <span className="menu-inside" onClick={() => handleKey("drafts")}> "Drafts" </span> section.</p>
                   <p className="start-para-details">First add names (your participants), set a title and click "New chat". Afterwards you can start writing your own chat.</p>
-                  <p className="start-para-details">If you like to edit the draft of your chat you can click "Save changes" when you are done editing, changes will be saved.</p>
-                  <p className="start-para-details">"Tags" and "Description" are the parts which will appear on the <span className="menu-inside" onClick={() => handleKey("userchats")}>"Userchats" </span> section when you published your chat.</p>
+                  <p className="start-para-details">While you are writung you should click "save" from time to time, so your text won't get lost. Same when you edit the draft of your chat.</p>
                 </div>
               </Collapse>
             </div>
@@ -120,8 +115,8 @@ export function MobileSitemap(props) {
               </div>
               <Collapse in={publish}>
                 <div className="menu-details" id="example-collapse-text">
-                  <p className="start-para-details">Once you are ready to publish your chat you go to <span className="menu-inside" onClick={() => handleKey("publish")}>"Publish" </span>, 
-                  there you have to set a "chatnumber" and a "date", then click on "Publish chat", it will be displayed in <span className="menu-inside" onClick={() => handleKey("userchats")}>"Userchats" </span>.</p>
+                  <p className="start-para-details">Once you are ready to publish your chat you go to <span className="menu-inside" onClick={() => handleKey("publish")}>"Publish" </span> and 
+                  set a "chatnumber" and a "date", then click on "Publish chat".</p>
                   <p className="start-para-details">You can also change the "chatnumber" "title", "date", "tags" and "description" of a published chat when you click on "Save changes" after you you made your changes.</p>
                 </div>
               </Collapse>
@@ -135,7 +130,7 @@ export function MobileSitemap(props) {
               <Collapse in={about}>
                 <div className="menu-details" id="example-collapse-text">
                 <p className="start-para-details">On <span className="menu-inside" onClick={() => handleKey("about")}>"About" </span> you will find a short explanation  
-                about why i created this app, get more technical informations about it and see the code behind on <a id="git" href="https://github.com/Florian2301/philomessenger">Github</a>.</p>
+                about why i created this app, get more technical informations about it and find a link to my github repository to see the code.</p>
                 </div>
               </Collapse>
             </div>
