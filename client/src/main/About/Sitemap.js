d0715d742a2f4c70dfa4e6ee109efbfeb18be205
Florian <67049493+Florian2301@users.noreply.github.com>
2021-01-15 11:00:10 +0100

    Text verkürzt

--- client/src/main/About/Sitemap.js
+++ client/src/main/About/Sitemap.js
@@ -11,7 +11,7 @@ export function Start() {
              text editor. In this section i will also post my own chats in the future</p>
             <p className="start-para">If you click on the title of a chat it will be displayed in <span className="start-para-inline">"Chat"</span>.</p>
             <p className="start-para">After you clicked the title, the "date" will change to a download link where you can download the chat as a PDF document.</p>
-            <p className="start-para">If you only hover over the title additional informations about the chat will appear: Tags and a brief description of the chat.</p>
+            <p className="start-para">If you hover over the title additional informations about the chat will appear: Tags and a brief description of the chat.</p>
             
             <h2 className="start-para-desktop">Userchats</h2>
             <p className="start-para">Under <span className="start-para-inline">"Userchats"</span> you can publish your own chats after registration. 
@@ -24,23 +24,21 @@ export function Start() {
             <h2 className="start-para-desktop">Login/Logout</h2>
             <p className="start-para">If you like to have a look "inside" the text editor, to see how to use it, you can go to <span className="start-para-inline">"Login"</span> and 
              log in with the given testuser credentials. Feel free to write, edit and publish a chat for testing purposes.</p>
-            <p className="start-para">The authorization is at the moment not a safe "https" process, a safe way to log in with your private email address will available in the future. 
-            This app is only for demonstration hosted at "heroku.com".</p>
             
             <h2 className="start-para-desktop">Drafts</h2>
             <p className="start-para">if you log in you can start writing a chat in the <span className="start-para-inline">"Drafts"</span> section.</p>
             <p className="start-para">First add names (your participants), set a title and click "New chat". Afterwards you can start writing your own chat.</p>
-            <p className="start-para">If you like to edit the draft of your chat you can click "Save changes" when you are done editing, changes will be saved.</p>
-            <p className="start-para">"Tags" and "Description" are the parts which will appear on the <span className="start-para-inline">"Userchats"</span> section when you published your chat.</p>
+            <p className="start-para">While you are writung you should click "save" from time to time, so your text won't get lost. Same when you edit the draft of your chat.</p>
+            
             
             <h2 className="start-para-desktop">Publish</h2>
-            <p className="start-para">Once you are ready to publish your chat you go to <span className="start-para-inline">"Publish"</span>, 
-            there you have to set a "chatnumber" and a "date", then click on "Publish chat", it will be displayed in <span className="start-para-inline">"Userchats"</span>.</p>
-            <p className="start-para">You can also change the "chatnumber" "title", "date", "tags" and "description" of a published chat when you click on "Save changes" after you you made your changes.</p>
+            <p className="start-para">Once you are ready to publish your chat you go to <span className="start-para-inline">"Publish"</span> and 
+            set a "chatnumber" and a "date", then click on "Publish chat", it will be displayed in <span className="start-para-inline">"Userchats"</span>.</p>
+            
             
             <h2 className="start-para-desktop">About</h2>
             <p className="start-para">On <span className="start-para-inline">"About"</span> you will find a short explanation
-            about why i created this app, get more technical informations about it and see the code behind on <a id="git" href="https://github.com/Florian2301/philomessenger">Github</a>.</p>
+            about why i created this app, get more technical informations about it and find a link to my github repository to see the code.</p>
             <br></br>
 
         </Panel>
