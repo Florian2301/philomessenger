8e0dead64c9648527262f5ff85bf6ebc687f0d49
Florian <67049493+Florian2301@users.noreply.github.com>
2021-01-20 18:16:47 +0100

    Transition eingefügt

--- client/src/responsive/MobileSitemap.js
+++ client/src/responsive/MobileSitemap.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { Collapse } from 'react-bootstrap'
 import Panel from '../elements/Panel'
 import { setKey } from '../redux/actions/user'
+import { CSSTransition, TransitionGroup } from 'react-transition-group' //CSS in Chat.css
+import { v4 as uuidv4 } from 'uuid'
 
 
 export function MobileSitemap(props) {
@@ -46,13 +48,17 @@ export function MobileSitemap(props) {
                 <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("history")}> History </span></p>
                 <p className="menu-link" onClick={() => toggleDetails("history")} aria-controls="example-collapse-text" aria-expanded={history}>{!history? "more..." : "less..."}</p>
               </div>
-              <Collapse in={history}>
-                <div className="menu-details" id="example-collapse-text">
-                  <p className="start-para-details">Under <span className="menu-inside" onClick={() => handleKey("history")}>"History" </span>
-                  you will find an introduction about how to use this chat editor. In this section i will also post my own chats in the future.</p>
-                  <p className="start-para-details">After you clicked the title, the "date" will change to a download link where you can download the chat as a PDF document.</p>
-                </div>
-              </Collapse>
+              <TransitionGroup>
+                <CSSTransition key={uuidv4()} timeout={100} classNames="transition-message">
+                  <Collapse in={history}>
+                    <div className="menu-details" id="example-collapse-text">
+                      <p className="start-para-details">Under <span className="menu-inside" onClick={() => handleKey("history")}>"History" </span>
+                      you will find an introduction about how to use this chat editor. In this section i will also post my own chats in the future.</p>
+                      <p className="start-para-details">After you clicked the title, the "date" will change to a download link where you can download the chat as a PDF document.</p>
+                    </div>
+                  </Collapse>
+                </CSSTransition>
+              </TransitionGroup>
             </div>
             
             <div>
@@ -60,12 +66,16 @@ export function MobileSitemap(props) {
                 <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("userchats")}> Userchats </span></p>
                 <p className="menu-link" onClick={() => toggleDetails("userchats")} aria-controls="example-collapse-text" aria-expanded={userchats}>{!userchats? "more..." : "less..."}</p>
               </div>
-              <Collapse in={userchats}>
-                <div className="menu-details" id="example-collapse-text">
-                  <p className="start-para-details">Under <span className="menu-inside" onClick={() => handleKey("userchats")}>"Userchats" </span> you can publish your own chats. 
-                  At the moment you can see example dialogues i quoted from the great Plato, to see how your chat could look like.</p>
-                </div>
-              </Collapse>
+              <TransitionGroup>
+                <CSSTransition key={uuidv4()} timeout={100} classNames="transition-message">
+                  <Collapse in={userchats}>
+                    <div className="menu-details" id="example-collapse-text">
+                      <p className="start-para-details">Under <span className="menu-inside" onClick={() => handleKey("userchats")}>"Userchats" </span> you can publish your own chats. 
+                      At the moment you can see example dialogues i quoted from the great Plato, to see how your chat could look like.</p>
+                    </div>
+                  </Collapse>
+                </CSSTransition>
+              </TransitionGroup>
             </div>
             
             <div>
@@ -73,12 +83,16 @@ export function MobileSitemap(props) {
                 <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("chat")}> Chat </span></p>
                 <p className="menu-link" onClick={() => toggleDetails("chat")} aria-controls="example-collapse-text" aria-expanded={chat}>{!chat? "more..." : "less..."}</p>
               </div>
-              <Collapse in={chat}>
-                <div className="menu-details" id="example-collapse-text">
-                  <p className="start-para-details">On <span className="menu-inside" onClick={() => handleKey("chat")}>"Chat" </span> the published chats will be displayed. When you are logged in, you will 
-                  write your chats in this section as well.</p>
-                </div>
-              </Collapse>
+              <TransitionGroup>
+                <CSSTransition key={uuidv4()} timeout={100} classNames="transition-message">
+                  <Collapse in={chat}>
+                    <div className="menu-details" id="example-collapse-text">
+                      <p className="start-para-details">On <span className="menu-inside" onClick={() => handleKey("chat")}>"Chat" </span> the published chats will be displayed. When you are logged in, you will 
+                      write your chats in this section as well.</p>
+                    </div>
+                  </Collapse>
+                </CSSTransition>
+              </TransitionGroup>
             </div>
 
             <div>
@@ -86,12 +100,16 @@ export function MobileSitemap(props) {
                 <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("login")}> Login/Logout</span></p>
                 <p className="menu-link" onClick={() => toggleDetails("login")} aria-controls="example-collapse-text" aria-expanded={login}>{!login? "more..." : "less..."}</p>
               </div>
-              <Collapse in={login}>
-                <div className="menu-details" id="example-collapse-text">
-                  <p className="start-para-details">If you like to have a look "inside" the chat editor, to see how to use it, you can go to <span className="menu-inside" onClick={() => handleKey("login")}>"Login" </span> and 
-                  log in with the given testuser credentials. Feel free to write, edit and publish a chat for testing purposes.</p>
-                </div>
-              </Collapse>
+              <TransitionGroup>
+                <CSSTransition key={uuidv4()} timeout={100} classNames="transition-message">
+                  <Collapse in={login}>
+                    <div className="menu-details" id="example-collapse-text">
+                      <p className="start-para-details">If you like to have a look "inside" the chat editor, to see how to use it, you can go to <span className="menu-inside" onClick={() => handleKey("login")}>"Login" </span> and 
+                      log in with the given testuser credentials. Feel free to write, edit and publish a chat for testing purposes.</p>
+                    </div>
+                  </Collapse>
+                </CSSTransition>
+              </TransitionGroup>
             </div>
 
             <div>
@@ -99,13 +117,17 @@ export function MobileSitemap(props) {
             <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("drafts")}> Drafts </span></p>
             <p className="menu-link" onClick={() => toggleDetails("drafts")} aria-controls="example-collapse-text" aria-expanded={drafts}>{!drafts? "more..." : "less..."}</p>
               </div>
-              <Collapse in={drafts}>
-                <div className="menu-details" id="example-collapse-text">
-                  <p className="start-para-details">if you log in you can start writing a chat in the <span className="menu-inside" onClick={() => handleKey("drafts")}> "Drafts" </span> section.</p>
-                  <p className="start-para-details">First add names (your participants), set a title and click "new chat". Afterwards you can start writing your own chat.</p>
-                  <p className="start-para-details">While you are writing you should click "save" from time to time, so your text won't get lost. Same when you edit the draft of your chat.</p>
-                </div>
-              </Collapse>
+              <TransitionGroup>
+                <CSSTransition key={uuidv4()} timeout={100} classNames="transition-message">
+                  <Collapse in={drafts}>
+                    <div className="menu-details" id="example-collapse-text">
+                      <p className="start-para-details">if you log in you can start writing a chat in the <span className="menu-inside" onClick={() => handleKey("drafts")}> "Drafts" </span> section.</p>
+                      <p className="start-para-details">First add names (your participants), set a title and click "New chat". Afterwards you can start writing your own chat.</p>
+                      <p className="start-para-details">While you are writing you should click "save" from time to time, so your text won't get lost. Same when you edit the draft of your chat.</p>
+                    </div>
+                  </Collapse>
+                </CSSTransition>
+              </TransitionGroup>
             </div>
 
             <div>
@@ -113,13 +135,17 @@ export function MobileSitemap(props) {
                 <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("publish")}> Publish </span></p>
                 <p className="menu-link" onClick={() => toggleDetails("publish")} aria-controls="example-collapse-text" aria-expanded={publish}>{!publish? "more..." : "less..."}</p>
               </div>
-              <Collapse in={publish}>
-                <div className="menu-details" id="example-collapse-text">
-                  <p className="start-para-details">Once you are ready to publish your chat you go to <span className="menu-inside" onClick={() => handleKey("publish")}>"Publish" </span> and 
-                  set a "chatnumber" and a "date", then click on "publish chat".</p>
-                  <p className="start-para-details">You can also change the "chatnumber" "title", "date", "tags" and "description" of a published chat when you click on "save changes" after you you made your changes.</p>
-                </div>
-              </Collapse>
+              <TransitionGroup>
+                <CSSTransition key={uuidv4()} timeout={100} classNames="transition-message">
+                  <Collapse in={publish}>
+                    <div className="menu-details" id="example-collapse-text">
+                      <p className="start-para-details">Once you are ready to publish your chat you go to <span className="menu-inside" onClick={() => handleKey("publish")}>"Publish" </span> and 
+                       set a "chatnumber" and a "date", then click on "Publish chat".</p>
+                      <p className="start-para-details">You can also change the "chatnumber" "title", "date", "tags" and "description" of a published chat when you click on "Save changes" after you you made your changes.</p>
+                    </div>
+                  </Collapse>
+                </CSSTransition>
+              </TransitionGroup>
             </div>
 
             <div>
@@ -127,12 +153,16 @@ export function MobileSitemap(props) {
                 <p className="start-para-mobile"><span className="menu" onClick={() => handleKey("about")}> About </span></p>
                 <p className="menu-link" onClick={() => toggleDetails("about")} aria-controls="example-collapse-text" aria-expanded={about}>{!about? "more..." : "less..."}</p>
               </div>
-              <Collapse in={about}>
-                <div className="menu-details" id="example-collapse-text">
-                <p className="start-para-details">On <span className="menu-inside" onClick={() => handleKey("about")}>"About" </span> you will find a short explanation  
-                about why i created this app, get more technical informations about it and find a link to my github repository to see the code.</p>
-                </div>
-              </Collapse>
+              <TransitionGroup>
+                <CSSTransition key={uuidv4()} timeout={100} classNames="transition-message">
+                  <Collapse in={about}>
+                    <div className="menu-details" id="example-collapse-text">
+                      <p className="start-para-details">On <span className="menu-inside" onClick={() => handleKey("about")}>"About" </span> you will find a short explanation  
+                      about why i created this app, get more technical informations about it and find a link to my github repository to see the code.</p>
+                    </div>
+                  </Collapse>
+                </CSSTransition>
+              </TransitionGroup>
             </div>
         </Panel>
     )
