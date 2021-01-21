eaa67dba9778a39b20e453885eb3757ea39bda17
Florian <67049493+Florian2301@users.noreply.github.com>
2021-01-21 13:25:30 +0100

    Styling

--- client/src/elements/PDF.js
+++ client/src/elements/PDF.js
@@ -9,11 +9,11 @@ import { v4 as uuidv4 } from 'uuid';
 const styles = StyleSheet.create({
   page: { backgroundColor: 'lightgrey' },
   header: { fontSize: 20, textAlign: 'center', margin: 30 },
-  info: { fontSize: 10, marginLeft: 10, marginTop: 30 },
-  infotext: { marginBottom: 5 },
-  philosopher: { fontSize: 10, marginTop: 5, marginLeft: 10 },
-  text: { fontSize: 15, marginBottom: 5, marginLeft: 10 },
-  color0: { backgroundColor: 'blue' },
+  user: { fontSize: 10, marginTop: 5},
+  info: { fontSize: 10, marginBottom: 30, textAlign: 'center', marginTop: 30 },
+  infotext: { marginBottom: 5, marginTop: 5},
+  philosopher: { fontSize: 10, marginTop: 5, marginLeft: 30 },
+  text: { fontSize: 15, marginBottom: 5, marginLeft: 30 },
 })
 
 // Create PDF document
@@ -21,7 +21,6 @@ const PDF = (props) => {
   let key
   let philosopher
   let text
-  let color
 
   return (
     <Document>
@@ -29,14 +28,13 @@ const PDF = (props) => {
     
         <View style={styles.header}>
           <Text>{props.title}</Text>
-          <Text style={styles.philosopher}>written by {props.user}</Text>
+          <Text style={styles.user}>written by {props.user}</Text>
         </View>
 
         {props.data.map((d) => {
           philosopher = d.name
           text = d.text
           key = uuidv4()
-          color = d.color
 
           return (
             <View key={key} color={color}>
@@ -47,10 +45,7 @@ const PDF = (props) => {
         })}
 
         <View style={styles.info}>
-          <Text style={styles.infotext}>Tags: {props.tags}</Text>
-          <Text style={styles.infotext}>Description: {props.description}</Text>
-          <Text style={styles.infotext}>Date: {props.date}</Text>
-          <Text style={styles.infotext}>Downloaded from: philomessenger.herokuapp.com</Text>
+          <Text style={styles.infotext}>Chat published: {props.date} on philomessenger.herokuapp.com</Text>
         </View>
 
       </Page>
